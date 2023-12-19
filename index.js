43e89f0066cd7e16c57c0df93f48a93a3c8eec25
Daniella Rodrigues <113942134+DanyRodriguesTi@users.noreply.github.com>
2023-12-19 15:05:58 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log-(-"Olá mundão!")
+console.log("Olá mundão!");
+
+let nomeHeroi = "Dany";
+let xpHeroi = 7300;
+let nivelHeroi;
+
+if (xpHeroi < 1000){
+    nivelHeroi = "Ferro";
+} else if (xpHeroi <= 2000){
+    nivelHeroi = "Bronze";
+} else if (xpHeroi <= 5000){
+    nivelHeroi = "Prata";
+} else if (xpHeroi <= 7000){
+    nivelHeroi = "Ouro";
+} else if(xpHeroi <= 8000){
+    nivelHeroi = "Platina";
+} else if (xpHeroi <= 9000){
+    nivelHeroi = "Ascendente";
+} else if (xpHeroi < 1000){
+    nivelHeroi = "Imortal";
+} else{
+    nivelHeroi = "Radiante"
+}
+
+console.log('O Heroi '+ nomeHeroi + ' está no nivel ' + nivelHeroi);
